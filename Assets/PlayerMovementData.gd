bf1193ec88f722f6ea477dc445be42b6e4d27b89
aowen7 <aowen@s.sfusd.edu>
2024-02-06 15:37:20 -0800

    fixed jumpie

--- Assets/PlayerMovementData.gd
+++ Assets/PlayerMovementData.gd
@@ -5,6 +5,6 @@ extends Resource
 @export var speed = 100.0
 @export var acceleration = 800.0 
 @export var friction = 1000.0
-@export var jumo_velocity = -300.0
+@export var jump_velocity = -300.0
 
 
