f4e3097c7c73545d2bf95bd2916589a4684123cd
aowen7 <aowen@s.sfusd.edu>
2024-02-06 15:30:25 -0800

    added playermovementdata script

---
+++ Assets/PlayerMovementData.gd
@@ -0,0 +1,10 @@
+class_name PlayerMovementData
+extends Resource
+
+
+@export var speed = 100.0
+@export var acceleration = 800.0 
+@export var friction = 1000.0
+@export var jumo_velocity = -300.0
+
+
